e837bc68c71fad3cdac570231386aca5b446ca39
kortey <richardkortey3@gmail.com>
2025-03-20 09:12:56 +0000

    added contact route

---
+++ app/routes/contact.jsx
@@ -0,0 +1,13 @@
+export function meta() {
+    return [
+      { title: "contact" },
+      { name: "description", content: "The hub for all your favorite events" },
+    ];
+  }
+  
+  export default function Contact() {
+    return (
+      <div className="hero">
+        <h1>contact page</h1>
+      </div>);
+  }
