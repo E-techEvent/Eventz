c64942ad314404849db93387791ddb0639d42633
RICHARD KORTEY <119814351+kortey@users.noreply.github.com>
2025-03-20 11:55:54 +0000

    Feature/added contact route (#7)
    * added contact route
    
    * contact page aded

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 export default [
     index("routes/home.jsx"),
     { path: "/events", file: "routes/events.jsx" },
+    { path: "/contact", file: "routes/contact.jsx" },
     { path: "/event/:id", file: "routes/event/$id.jsx" },
     { path: "/dashboard/user/:userId", file: "routes/dashboards/users/$userId.jsx" },
     { path: "/dashboard/admin/:userId", file: "routes/dashboards/admin/$userId.jsx" },
---
+++ app/routes/contact.jsx
@@ -0,0 +1,13 @@
+export function meta() {
+    return [
+      { title: "contact" },
+      { name: "description", content: "The hub for all your favorite events" },
+    ];
+  }
+  
+  export default function Contact() {
+    return (
+      <div className="hero">
+        <h1>contact</h1>
+      </div>);
+  }
